68b0f300f56a374cbc893f8e4d1221f9b640a533
Daniel García <workspacedaniel2121@gmail.com>
2024-11-15 20:04:01 +0100

    Listo Calisto

--- src/controllers/addRecipe.controller.js
+++ src/controllers/addRecipe.controller.js
@@ -1,8 +1,47 @@
 import views from '../views/addRecipe.html';
 
 export default () => {
-  const div = document.createElement('div');
-  div.innerHTML = views;
+  const divElement = document.createElement('div');
+  divElement.innerHTML = views;
 
-  return div;
+  const form = divElement.querySelector('#recipe-form');
+
+  // Handle form Submit
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // data Form
+    const name = form.querySelector('#name').value;
+    const ingredients = form.querySelector('#ingredients').value.split(',');
+    const time = form.querySelector('#time').value;
+    const instructions = form.querySelector('#instructions').value;
+    const imageUrl = form.querySelector('#image').value;
+
+    // Validate URL img
+    const imageRegex = /\.(jpeg|jpg|png|gif)$/i;
+    if (!imageRegex.test(imageUrl)) {
+      alert('La URL de la imagen debe ser válida.');
+      return;
+    }
+
+    const newRecipe = {
+      id: Date.now(),
+      name,
+      ingredients,
+      time,
+      instructions,
+      imageUrl,
+    };
+
+    // Add new recipe to local storage
+    const storedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+    storedRecipes.push(newRecipe);
+    localStorage.setItem('recipes', JSON.stringify(storedRecipes));
+
+    form.reset();
+
+    alert('Receta agregada correctamente!');
+  });
+
+  return divElement;
 };
